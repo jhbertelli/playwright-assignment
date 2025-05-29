c7d7d29b45d0ea61ed3b54218ba02a8b96b5f800
Jose Augusto <luke12jose@hotmail.com>
2025-05-29 11:49:20 -0300

    added login playwright tests

--- playwright/login.spec.ts
+++ playwright/login.spec.ts
@@ -1,5 +1,21 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
-test.skip('render login form', async ({ page }) => {})
+test.skip('render login form', async ({ page }) => {
+    await page.goto('/login')
 
-test.skip('can redirect to register page', async ({ page }) => {})
+    await expect(page).toHaveTitle('Login')
+
+    await expect(page.getByRole('textbox', { name: 'E-mail'})).toBeVisible()
+    await expect(page.getByRole('textbox', { name: 'Senha'})).toBeVisible()
+})
+
+test.skip('can redirect to register page', async ({ page }) => {
+    await page.goto('/login')
+
+    const redirectToRegisterButton = page.getByRole('button', { name: 'Não possui uma conta?'})
+
+    await expect(redirectToRegisterButton).toBeVisible()
+    await redirectToRegisterButton.click()
+
+    await expect(page).toHaveTitle('Cadastro')
+})
